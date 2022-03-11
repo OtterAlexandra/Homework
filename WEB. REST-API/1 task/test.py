3a8eb335706f44340b1c3342a099621c4a090ddc
OtterAlexandra <otter9919@mail.ru>
2022-03-11 11:08:54 +0300

    Create test.py

---
+++ WEB. REST-API/1 task/test.py
@@ -0,0 +1,7 @@
+from requests import get
+
+print(get('http://localhost:5000/api/jobs').json())
+print(get('http://localhost:5000/api/jobs/1').json())
+print(get('http://localhost:5000/api/news/876557').json())
+print(get('http://localhost:5000/api/news/try').json())
+
